fcfed573b32d073cdd9ac83e54fd3da9304329d3
Darius Daniel <dariusdaniel771@gmail.com>
2024-04-29 20:14:20 +0100

    Set up new Adonis project

---
+++ inertia/pages/home.tsx
@@ -0,0 +1,9 @@
+import { Head } from '@inertiajs/react'
+
+export default function Home(props: { version: number }) {
+  return (
+    <>
+      <Head title="Spaces: Note Taking Web App" />
+    </>
+  )
+}
